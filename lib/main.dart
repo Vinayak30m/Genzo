de8ac105a46888307fd0fd991aa9b9a88f5a97a0
Naman Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2024-02-09 20:07:23 +0530

    commit from naman for test

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
     );
   }
 }
