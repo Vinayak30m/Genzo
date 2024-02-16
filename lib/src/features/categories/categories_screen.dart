4fb3ac01a23ffddcc1c6efbe010ed43add05bccf
Naman Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2024-02-16 09:58:35 +0530

    Add import for CategoriesScreen and navigate to it when a category is pressed

--- lib/src/features/categories/categories_screen.dart
+++ lib/src/features/categories/categories_screen.dart
@@ -1,49 +1,135 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:genzo/src/commons/widgets/custom_appbar.dart';
 import 'package:genzo/src/commons/widgets/custom_textbutton_widget.dart';
 import 'package:genzo/src/features/categories/data/course_card_data/course_card_data.dart';
 import 'package:genzo/src/features/home/widgets/unique_pick_card_widget.dart';
 import 'package:genzo/src/res/colors.dart';
+import 'package:genzo/src/utils/screen_dimensions.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
 
-class CategoriesScreen extends StatefulWidget {
-  final String categoryText;
-  const CategoriesScreen({required this.categoryText,super.key});
+class CategoriesScreen extends StatelessWidget {
+  final String categoryName;
 
-  @override
-  State<CategoriesScreen> createState() => _CategoriesScreenState();
-}
+  const CategoriesScreen({super.key, required this.categoryName});
 
-class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-          customTextButton: CustomTextButtonWidget(
-            buttonText: 'Back',
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            textcolor: AppColor.textbuttonColor,
-            fontSize: 14,
+    final Stream<QuerySnapshot> _courseStream = FirebaseFirestore.instance
+        .collection('courses')
+        .where('courseCategory', isEqualTo: categoryName)
+        .snapshots();
+
+    ScreenDimensions screenDimensions = ScreenDimensions(context);
+    return StreamBuilder<QuerySnapshot>(
+      stream: _courseStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Scaffold(
+          appBar: CustomAppBar(
+              customTextButton: CustomTextButtonWidget(
+                buttonText: 'Back',
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                textcolor: AppColor.textbuttonColor,
+                fontSize: 14,
+              ),
+              staticText: categoryName),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: ((context, index) {
+                      final courseData = snapshot.data!.docs[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          borderOnForeground: true,
+                          child: Container(
+                            height: screenDimensions.screenHeight * 0.12,
+                            decoration: BoxDecoration(
+                                // boxShadow: <BoxShadow>[
+                                //   BoxShadow(
+                                //     color: Colors.black54,
+                                //     blurRadius: 15.0,
+                                //     offset: Offset(0.0, 1),
+                                //   )
+                                // ],
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                    color:
+                                        AppColor.secondaryelevatedButtonColor,
+                                    width: 1)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                    width: screenDimensions.screenWidth * 0.4,
+                                    height:
+                                        screenDimensions.screenHeight * 0.15,
+                                    child: Image.network(
+                                      courseData['imageUrl'],
+                                      fit: BoxFit.fill,
+                                    )),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      courseData['courseTitle'],
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    // SizedBox(
+                                    //   height: 10,
+                                    // ),
+
+                                    Container(
+                                      width: screenDimensions.screenWidth * 0.4,
+                                      // height: screenDimensions.screenHeight * 0.4,
+                                      child: Text(
+                                        courseData['courseDescription'],
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          fontSize: 8,
+                                        ),
+                                      ),
+                                    ),
+                                    Text('₹ ${courseData['coursePrice']}')
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    })),
+              )
+            ],
           ),
-          staticText: widget.categoryText),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: ListView.builder(
-                itemCount: courseData.length,
-                itemBuilder: ((context, index) {
-                  return Card(
-                    borderOnForeground: true,
-                    child: UniquePicksCardWidget(
-                        course: courseData[index % courseData.length]),
-                  );
-                })),
-          )
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/src/features/categories/category_widget.dart
+++ lib/src/features/categories/category_widget.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:genzo/src/features/categories/categories_screen.dart';
 import 'package:genzo/src/features/home/widgets/categorybox_widget.dart';
 
 class CategoryWidget extends StatefulWidget {
@@ -18,38 +20,50 @@ class _CategoryWidgetState extends State<CategoryWidget> {
     final Stream<QuerySnapshot> _categoryStream =
         FirebaseFirestore.instance.collection('categories').snapshots();
     return StreamBuilder<QuerySnapshot>(
-        stream: _categoryStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
+      stream: _categoryStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading categories...");
-          }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading categories...");
+        }
 
-          return Container(
-            height: 200,
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  childAspectRatio: 1 / 1.1,
-                ),
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final categoryData = snapshot.data!.docs[index];
-                  return CategoryBoxWidget(
-                    image: categoryData['image'],
-                    text: categoryData['categoryName'],
-                    onPressed: () {
-                      print(categoryData['categoryName']);
-                      setState(() {
-                        _selectedCategory = categoryData['categoryName'];
-                      });
-                    },
+        return Container(
+          height: 200,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: 1 / 1.1,
+            ),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final categoryData = snapshot.data!.docs[index];
+              return CategoryBoxWidget(
+                image: categoryData['image'],
+                text: categoryData['categoryName'],
+                onPressed: () {
+                  print(categoryData['categoryName']);
+                  setState(() {
+                    _selectedCategory = categoryData['categoryName'];
+                  });
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return (CategoriesScreen(
+                          categoryName: categoryData['categoryName'],
+                        ));
+                      },
+                    ),
                   );
-                }),
-          );
-        });
+                },
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 }
