fc2cc0d1ecaf8ccddf4fcaca782cc25e91618723
Naman Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2024-02-17 00:35:52 +0530

    minor changes

---
+++ lib/src/features/home/course_detail/course_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CourseDetailScreen extends StatefulWidget {
+  const CourseDetailScreen({super.key});
+
+  @override
+  State<CourseDetailScreen> createState() => _CourseDetailScreenState();
+}
+
+class _CourseDetailScreenState extends State<CourseDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
